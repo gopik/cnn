67327bcf2aecd7a29c4d8bb20ee2d06044f34aba
Gopi Madabhushi <gopik@users.noreply.github.com>
2017-09-14 11:47:53 +0530

    add flags for training config

--- mnist_cnn_scratch.py
+++ mnist_cnn_scratch.py
@@ -1,6 +1,18 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser(description='Train CNN for MNIST')
+parser.add_argument('--save_model_dir', help='Path to save exported model. Model will be exported only if provided')
+parser.add_argument('--num_training_steps', type=int, default=1000, help='Number of steps to run the training for')
+parser.add_argument('--checkpoint_dir',
+                    help='Path to load/save checkpoint. If provided, latest checkpoint will be loaded from here')
+parser.add_argument('--checkpoint_every', type=int, help='Num iterations to checkpoint after', default=100)
+parser.add_argument('--batch_size', type=int, default=50)
+
+args = parser.parse_args()
 
 mnist = input_data.read_data_sets('MNIST-data', one_hot=True)
 
@@ -54,7 +66,7 @@ with tf.name_scope('fc1'):
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-with tf.name_scope('droput'):
+with tf.name_scope('dropout'):
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
@@ -66,45 +78,65 @@ with tf.name_scope('readout'):
 summary_writer = tf.summary.FileWriter('/tmp/mnist_scratch', graph=tf.get_default_graph())
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+global_step = tf.Variable(name='global_step', initial_value=0, dtype=tf.int32)
+rate = tf.train.exponential_decay(1e-5, global_step, decay_rate=0.99, decay_steps=10)
 
-training_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1), name='compare_prediction')
+training_step = tf.train.AdamOptimizer(rate).minimize(cross_entropy, global_step=global_step)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='compare_prediction')
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-export_dir = '/tmp/cnn_model'
-builder = tf.saved_model.builder.SavedModelBuilder(export_dir=export_dir)
-classification_inputs = tf.saved_model.utils.build_tensor_info(tf.placeholder(tf.string))
-classification_outputs = tf.saved_model.utils.build_tensor_info(tf.constant(dtype=tf.int32, value=np.zeros(10)))
-classification_output_scores = tf.saved_model.utils.build_tensor_info(tf.constant(dtype='float', value=0.0))
 
-classification_signature = tf.saved_model.signature_def_utils.build_signature_def(
-    inputs={
-        tf.saved_model.signature_constants.CLASSIFY_INPUTS: classification_inputs,
-    },
-    outputs={
-        tf.saved_model.signature_constants.CLASSIFY_OUTPUT_CLASSES: classification_outputs,
-        tf.saved_model.signature_constants.CLASSIFY_OUTPUT_SCORES: classification_output_scores,
-    },
-    method_name=tf.saved_model.signature_constants.CLASSIFY_METHOD_NAME)
+def export_saved_model(export_dir, session, as_text):
+    builder = tf.saved_model.builder.SavedModelBuilder(export_dir=export_dir)
+    classification_inputs = tf.saved_model.utils.build_tensor_info(tf.placeholder(tf.string))
+    classification_outputs = tf.saved_model.utils.build_tensor_info(tf.constant(dtype=tf.int32, value=np.zeros(10)))
+    classification_output_scores = tf.saved_model.utils.build_tensor_info(tf.constant(dtype='float', value=0.0))
 
-saver = tf.train.Saver()
-with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    for i in range(200):
-        batch = mnist.train.next_batch(50)
-        if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={
-                x: batch[0], y_: batch[1], keep_prob: 1.0
-            })
-            print("step %d, accuracy=%f" % (i, train_accuracy))
-        training_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-        if i % 50 == 0:
-            saver.save(session, '/tmp/model.%d.ckpt' % i)
-    test_accuracy = accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0
-    })
+    classification_signature = tf.saved_model.signature_def_utils.build_signature_def(
+        inputs={
+            tf.saved_model.signature_constants.CLASSIFY_INPUTS: classification_inputs,
+        },
+        outputs={
+            tf.saved_model.signature_constants.CLASSIFY_OUTPUT_CLASSES: classification_outputs,
+            tf.saved_model.signature_constants.CLASSIFY_OUTPUT_SCORES: classification_output_scores,
+        },
+        method_name=tf.saved_model.signature_constants.CLASSIFY_METHOD_NAME)
+
     builder.add_meta_graph_and_variables(sess=session, signature_def_map={
         tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: classification_signature},
                                          tags=[tf.saved_model.tag_constants.SERVING])
-    builder.save(as_text=True)
+    builder.save(as_text=as_text)
+
+
+saver = tf.train.Saver()
+
+
+CHECKPOINT_FILE_NAME = 'checkpoint'
+
+with tf.Session() as sess:
+    latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir=args.checkpoint_dir)
+    if latest_checkpoint:
+        saver.restore(sess, latest_checkpoint)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    for i in range(args.num_training_steps):
+        batch = mnist.train.next_batch(args.batch_size)
+        if i % args.checkpoint_every == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x: batch[0], y_: batch[1], keep_prob: 1.0
+            })
+            global_step_index = sess.run(global_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+            if args.checkpoint_dir:
+                saver.save(sess, os.path.join(args.checkpoint_dir, CHECKPOINT_FILE_NAME), global_step=global_step)
+            print("step %d, accuracy=%f, global_step=%d" % (i, train_accuracy, global_step_index))
+
+        sess.run(training_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    test_accuracy = accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0
+    })
+
     print("test accuracy=%f" % test_accuracy)
+    if args.save_model_dir:
+        export_saved_model(args.save_model_dir, session=sess, as_text=True)
