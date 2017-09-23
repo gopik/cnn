03214cce4669ff8407ee31d697d750e9f9e0c471
Gopi Madabhushi <gopik@users.noreply.github.com>
2017-09-23 13:04:00 +0530

    test images

--- font_predict.py
+++ font_predict.py
@@ -3,6 +3,8 @@ from scipy import ndimage
 from recognizer import Recognizer
 import numpy as np
 
+classes = ['Unknown', 'L', 'S', 'H', 'G', '9', 'V', '8', 'Q', '5', 'T', '7', 'C', '6', 'O', 'M', 'P', 'Z', 'N',
+           'E', '3', 'X', '4', 'I', 'U', 'D', 'W', 'R', 'J', 'B', 'K', 'Y', 'A', '2', '1', '0', 'F']
 
 def main():
     parser = argparse.ArgumentParser(description='Train CNN for MNIST')
@@ -12,8 +14,14 @@ def main():
 
     image = ndimage.imread(args.input_image, flatten=True)
     with Recognizer(args.save_model_dir) as recognizer:
-        print(recognizer.predict(image[np.newaxis, :, :, np.newaxis]))
-        print(np.argmax(recognizer.predict(image[np.newaxis, :, :, np.newaxis])))
+        reshaped_image = image[np.newaxis, :, :, np.newaxis]
+        prediction = recognizer.predict(reshaped_image)
+        print(prediction)
+
+        class_index = np.argmax(prediction)
+        print(class_index)
+
+        print(classes[class_index])
 
 
 if __name__ == '__main__':
