a4aead5d6387204d2a77030f9e1d61bc400f396e
Gopi Madabhushi <gopik@users.noreply.github.com>
2017-09-23 12:24:05 +0530

    compute mean image and save in the graph

--- char_detector_train.py
+++ char_detector_train.py
@@ -63,66 +63,16 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-x = tf.placeholder(tf.float32, shape=[None, 1200], name='train_images')
-y_ = tf.placeholder(tf.float32, shape=[None, 37], name='train_labels')
+default_graph = tf.Graph()
+
+with default_graph.as_default():
+    x = tf.placeholder(tf.float32, shape=[None, 1200], name='train_images')
+    y_ = tf.placeholder(tf.float32, shape=[None, 37], name='train_labels')
 
 # For tf.variable_scope vs tf.name_scope,
 #  see https://stackoverflow.com/questions/35919020/whats-the-difference-of-name-scope-and-a-variable-scope-in-tensorflow
 
-with tf.name_scope('reshape'):
-    # with padding, the image size is 40x32
-
-    x_mean = tf.reduce_mean(x, axis=0)
-    x_input = tf.reshape(tf.subtract(x, x_mean, name='subract_mean'), [-1, 40, 30, 1], name='reshaped_images')
-
-    x_image = tf.pad(x_input, [[0, 0], [0, 0], [1, 1], [0, 0]])
-
-with tf.name_scope('conv1'):
-    W_conv1 = weight_variable([3, 3, 1, 32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-
-with tf.name_scope('pool1'):
-    h_pool1 = max_pool_2x2(h_conv1)
-
-with tf.name_scope('conv2'):
-    W_conv2 = weight_variable([3, 3, 32, 64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-
-with tf.name_scope('pool2'):
-    h_pool2 = max_pool_2x2(h_conv2)
-
-with tf.name_scope('fc1'):
-    W_fc1 = weight_variable([10 * 8 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-    h_pool3_flat = tf.reshape(h_pool2, [-1, 10 * 8 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
-
-with tf.name_scope('dropout'):
-    keep_prob = tf.placeholder_with_default(1.0, name='keep_prob', shape=())
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-with tf.name_scope('readout'):
-    W_fc2 = weight_variable([1024, 37])
-    b_fc2 = bias_variable([37])
-    y_conv = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2, name='y_conv')
-
-softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
-cross_entropy = tf.reduce_mean(softmax_cross_entropy, name='cross_entropy')
-global_step = tf.Variable(name='global_step', initial_value=0, dtype=tf.int32)
-rate = tf.train.exponential_decay(1e-5, global_step, decay_rate=0.99, decay_steps=100)
-
-training_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy, global_step=global_step)
-
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='compare_prediction')
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar(name='accuracy', tensor=accuracy)
-tf.summary.scalar(name='cross_entropy_loss', tensor=cross_entropy)
-
-default_graph = tf.get_default_graph()
 dataset_graph = tf.Graph()
-
 
 with dataset_graph.as_default():
     train_dataset = TFRecordDataset('/tmp/fonts/out/train-00000-of-00001').map(get_feature).repeat(5).batch(
@@ -135,9 +85,78 @@ with dataset_graph.as_default():
 
     val_iterator = val_dataset.make_one_shot_iterator()
     val_next_batch = val_iterator.get_next()
+    mean_dataset = TFRecordDataset('/tmp/fonts/out/train-00000-of-00001').map(
+        get_feature).make_one_shot_iterator().get_next()
 
-summary_writer_train = tf.summary.FileWriter(os.path.join(args.logdir, 'train'), graph=default_graph)
-summary_writer_val = tf.summary.FileWriter(os.path.join(args.logdir, 'validation'), graph=default_graph)
+with tf.Session(graph=dataset_graph) as sess:
+    count = 0
+    image_sum = None
+    try:
+        image, label = sess.run(mean_dataset)
+        if image_sum is None:
+            image_sum = image
+            count = 1
+        else:
+            image_sum += image
+            count += 1
+    except():
+        pass
+
+with default_graph.as_default():
+    mean_image = tf.reshape(tf.constant(image_sum * 1.0 / count, dtype=tf.float32), [-1, 40, 30, 1])
+
+    with tf.name_scope('reshape'):
+        print(x.get_shape())
+        x_input = tf.reshape(x, [-1, 40, 30, 1], name='reshaped_images')
+
+        x_image_sub_mean = x_input - mean_image
+        x_image = tf.pad(x_image_sub_mean, [[0, 0], [0, 0], [1, 1], [0, 0]])
+
+    with tf.name_scope('conv1'):
+        W_conv1 = weight_variable([3, 3, 1, 32])
+        b_conv1 = bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+    with tf.name_scope('pool1'):
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.name_scope('conv2'):
+        W_conv2 = weight_variable([3, 3, 32, 64])
+        b_conv2 = bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+    with tf.name_scope('pool2'):
+        h_pool2 = max_pool_2x2(h_conv2)
+
+    with tf.name_scope('fc1'):
+        W_fc1 = weight_variable([10 * 8 * 64, 1024])
+        b_fc1 = bias_variable([1024])
+        h_pool3_flat = tf.reshape(h_pool2, [-1, 10 * 8 * 64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+
+    with tf.name_scope('dropout'):
+        keep_prob = tf.placeholder_with_default(1.0, name='keep_prob', shape=())
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    with tf.name_scope('readout'):
+        W_fc2 = weight_variable([1024, 37])
+        b_fc2 = bias_variable([37])
+        y_conv = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2, name='y_conv')
+
+    softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
+    cross_entropy = tf.reduce_mean(softmax_cross_entropy, name='cross_entropy')
+    global_step = tf.Variable(name='global_step', initial_value=0, dtype=tf.int32)
+    rate = tf.train.exponential_decay(1e-5, global_step, decay_rate=0.99, decay_steps=100)
+
+    training_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy, global_step=global_step)
+
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='compare_prediction')
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar(name='accuracy', tensor=accuracy)
+    tf.summary.scalar(name='cross_entropy_loss', tensor=cross_entropy)
+
+    summary_writer_train = tf.summary.FileWriter(os.path.join(args.logdir, 'train'), graph=default_graph)
+    summary_writer_val = tf.summary.FileWriter(os.path.join(args.logdir, 'validation'), graph=default_graph)
 
 
 def export_saved_model(export_dir, session, as_text):
@@ -160,7 +179,8 @@ def export_saved_model(export_dir, session, as_text):
     builder.save(as_text=as_text)
 
 
-saver = tf.train.Saver()
+with default_graph.as_default():
+    saver = tf.train.Saver()
 
 CHECKPOINT_FILE_NAME = 'checkpoint'
 
@@ -172,7 +192,8 @@ with tf.Session(graph=dataset_graph) as dataset_session:
         else:
             sess.run(tf.global_variables_initializer())
 
-        # val_iterator = val_dataset.make_one_shot_iterator()
+        validation_images, validation_labels = dataset_session.run(val_next_batch)
+
         for i in range(args.num_training_steps):
             images, labels = dataset_session.run(train_next_batch)
 
@@ -198,11 +219,10 @@ with tf.Session(graph=dataset_graph) as dataset_session:
             # print(cross_entropy_val)
 
             summary_writer_train.add_summary(summaries, step_id)
-            # validation_images, validation_labels = sess.run(val_next_batch)
-            #
-            # summaries, _ = sess.run([tf.summary.merge_all(), training_step],
-            #                         feed_dict={x: validation_images, y_: validation_labels, keep_prob: 1.0})
-            # summary_writer_val.add_summary(summaries, step_id)
+
+            summaries = sess.run(tf.summary.merge_all(),
+                                 feed_dict={x: validation_images, y_: validation_labels, keep_prob: 1.0})
+            summary_writer_val.add_summary(summaries, step_id)
 
         if args.save_model_dir:
             export_saved_model(args.save_model_dir, session=sess, as_text=True)
